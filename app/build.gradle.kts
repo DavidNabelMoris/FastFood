6ace332ce8bdfbb73a451730f1d2bcf718612bc5
Ton Nom <jfjjfjfdjj@gmail.com>
2025-10-02 09:00:20 +0200

    change hello world

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.fastfood"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.fastfood"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
