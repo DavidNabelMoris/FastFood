51b1b4bc322cfdfe6f2e0667c2ceebb736d81944
David <David.Nabel-Moris@etu.univ-smb.fr>
2025-10-14 21:41:32 +0200

    nothing

--- app/src/main/java/com/example/fastfood/MainActivity.kt
+++ app/src/main/java/com/example/fastfood/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.fastfood.composable.RestoListScreen
 import com.example.fastfood.ui.theme.FastFoodTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FastFoodTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                        RestoListScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -38,10 +36,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+/*fun GreetingPreview() {
     FastFoodTheme {
         Greeting("Android")
+    }
+}*/
+fun RestoListPreview() {
+    FastFoodTheme {
+        RestoListScreen()
     }
 }
